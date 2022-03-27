587155272e1dc2dd2bb676e0aedfcb601424ee0e
budhalantara <budhalantara@gmail.com>
2022-03-27 20:10:15 +0800

    add dynamic port

--- web/api/cli.js
+++ web/api/cli.js
@@ -3,11 +3,16 @@ const fs = require('fs')
 const { execSync } = require('child_process')
 
 const opts = require('command-line-args')([
-  { name: 'port', alias: 'p', type: Number }
+  { name: 'port', alias: 'p', type: Number },
+  { name: 'expose', alias: 'e', type: Boolean }
 ])
 
 if (opts.port) {
   process.env.PORT = opts.port
+}
+
+if (opts.expose) {
+  process.env.HOST = '0.0.0.0'
 }
 
 if (!fs.existsSync(path.resolve(__dirname, '../public/build'))) {
